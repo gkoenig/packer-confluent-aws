24dc2246189a5719f047d554bd72c57aff84af85
Gerd Koenig <koenig.bodensee@googlemail.com>
2019-07-07 15:33:01 +0200

    deleted useless key

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -48,11 +48,6 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-resource "aws_key_pair" "MemberDay-temp-key" {
-  key_name = "MemberDay-temp-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDHpzdTKCAwCLbyuxVe0Y+4EeI1J3h8ErIOJhDxS3HoJ2G/MF3rnNQrlcslD2WPI8OtVl00SFPLDXB85vX3ZvHIRyJYa7UiCsIzKyhnRFtcpklcPMZQ5dViz5YVFn1tvOvPH7iILLhw/iAtPcx04YNi/CDCHYg/CeO6d6PY4ooLbJaJC8rHFmkhvFaqWxNxKScFiFSQy6d5WQeHLmBYwJGVvkZnSlkUl/CZjiuPVxk29uyeXZZntjR9i9m+VLy/j1/jOyK/ymrfau90U/bsm13jvuaBOe+tQpR1/t2kQOsz0Zrkd4+ttXvkqGinumMQuZudXYJDQZMoT47dEE6lEGkQzEoe4GDnPOo0OBmJDuGTlFMonOwv/wmsPnNHIceqXw2C/VsBijJ4T6ghtPn4mw1NHwzvmylVx4GZe44BOy6qxQFWOOKNwGuGzdNTK9Ve6Nm6r/ap5OSJMzPOoOZjasmr3W9q6Er9XKVYXMh9+QZeSqgJVAWmqJjmcdwTO9drv/s= gerd@zenbook"
-}
-
 output "ec2_global_ips" {
   value = ["${aws_instance.my-instance.*.public_ip}"]
 }
